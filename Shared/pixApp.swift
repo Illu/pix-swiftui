cd794d85e42333d4bc19f068b3968262f31fadd0
Maxime Nory <maxime.n@kimoby.com>
2021-11-10 13:16:34 -0500

    Initial Commit

---
+++ Shared/pixApp.swift
@@ -0,0 +1,17 @@
+//
+//  pixApp.swift
+//  Shared
+//
+//  Created by Maxime Nory on 2021-11-10.
+//
+
+import SwiftUI
+
+@main
+struct pixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
