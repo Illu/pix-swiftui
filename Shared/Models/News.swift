2125dc0faaf64c301c561bbf4e8bbf2933b32725
Maxime Nory <mail@maximenory.com>
2022-03-09 21:30:43 -0500

    feat: load data in news screen

---
+++ Shared/Models/News.swift
@@ -0,0 +1,14 @@
+//
+//  News.swift
+//  pix
+//
+//  Created by Maxime Nory on 2022-03-09.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct News: Codable, Hashable {
+	var desc: String
+	var title: Int
+}
--- Shared/Screens/NewsScreen.swift
+++ Shared/Screens/NewsScreen.swift
@@ -6,12 +6,54 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
 
 struct NewsScreen: View {
+	
+	@State var newsData: News? = nil
+	@State var state: States = States.IDLE
+	
+	var db = Firestore.firestore()
+	
+	func loadData () {
+		self.state = States.LOADING
+		let docRef = self.db.collection("News").document("current")
+		docRef.getDocument { document, error in
+			if let error = error as NSError? {
+				print ("error: \(error.localizedDescription)")
+				self.state = States.ERROR
+			}
+			else {
+				if let document = document {
+					do {
+						let newsData = try document.data(as: News.self)
+						self.newsData = newsData ?? nil
+						self.state = States.SUCCESS
+					}
+					catch {
+						print(error)
+						self.state = States.ERROR
+					}
+				}
+			}
+		}
+	}
+
+	
     var body: some View {
         VStack {
-            Text(":)")
-        }.navigationTitle("What's new")
+			if (state == States.LOADING) {
+				ProgressView()
+			} else if (newsData == nil) {
+				Text("Something went wrong while retrieving the latest news 😥")
+			}
+			if (newsData != nil) {
+				Text("\(newsData!.title)")
+				Text("\(newsData!.desc)")
+			}
+		}
+		.navigationTitle("What's new")
+		.onAppear{self.loadData()}
     }
 }
 
