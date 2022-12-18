802b2655154929e7c19157fb71c58fe8323cec31
Maxime Nory <mail@maximenory.com>
2022-12-17 19:53:32 -0500

    feat: new color palettes

--- Shared/Colors/Palettes.swift
+++ Shared/Colors/Palettes.swift
@@ -8,69 +8,142 @@
 import Foundation
 
 struct Palette {
-    var name: String
-    var colors: [String]
+	var name: String
+	var colors: [String]
 }
 
 let Palettes: [Palette] = [
-    Palette(
-        name: "Classic",
-        colors: [
-            "#2B2D42",
-            "#FFFFFF",
-            "#ED6A5A",
-            "#FFB800",
-            "#35CE8D",
-            "#4DB3FF",
-            "#0085FF"
-        ]
-    ),
-    Palette(
-        name: "Fall",
-        colors: [
-            "#291517",
-            "#5A1F31",
-            "#A47973",
-            "#F79A32",
-            "#AD4D00",
-            "#0A4E35",
-            "#859B8E"
-        ]
-    ),
-    Palette(
-        name: "Ocean",
-        colors: [
-            "#023450",
-            "#076194",
-            "#6493AD",
-            "#72ACAE",
-            "#B4DADB",
-            "#F2F1F7",
-            "#B2AFCC"
-        ]
-    ),
-    Palette(
-        name: "Greens",
-        colors: [
-            "#DFEEC5",
-            "#BAD87C",
-            "#BDFF00",
-            "#73DB21",
-            "#55B808",
-            "#188B46",
-            "#01672A"
-        ]
-    ),
-    Palette(
-        name: "Skin",
-        colors: [
-            "#F9F4F0",
-            "#FAEBDE",
-            "#F8ECCF",
-            "#E6C6A2",
-            "#795A3D",
-            "#5F3D1F",
-            "#402812"
-        ]
-    )
+	Palette(
+		name: "Classic",
+		colors: [
+			"#2B2D42",
+			"#FFFFFF",
+			"#ED6A5A",
+			"#FFB800",
+			"#35CE8D",
+			"#4DB3FF",
+			"#0085FF"
+		]
+	),
+	Palette(
+		name: "Sakura",
+		colors: [
+			"#9F82B0",
+			"#CDB4DB",
+			"#FFC8DD",
+			"#FFAFCC",
+			"#BDE0FE",
+			"#A2D2FF",
+			"#68ABE9"
+		]
+	),
+	Palette(
+		name: "Groovy 70s",
+		colors: [
+			"#686346",
+			"#75C7AE",
+			"#E9C46A",
+			"#F8EAAF",
+			"#F4A261",
+			"#E7581A",
+			"#5B3A29"
+		]
+	),
+	Palette(
+		name: "Spring",
+		colors: [
+			"#FD6842",
+			"#FFB7A2",
+			"#F5CBB5",
+			"#F8F3E8",
+			"#D0DFE5",
+			"#91A066",
+			"#FCC465"
+		]
+	),
+	Palette(
+		name: "Summer",
+		colors: [
+			"#48B9A82",
+			"#BAF1E4",
+			"#FA3C5A",
+			"#FA990D",
+			"#FECC47",
+			"#AFD204",
+			"#294701"
+		]
+	),
+	Palette(
+		name: "Fall",
+		colors: [
+			"#291517",
+			"#5A1F31",
+			"#A47973",
+			"#F79A32",
+			"#AD4D00",
+			"#0A4E35",
+			"#859B8E"
+		]
+	),
+	Palette(
+		name: "Winter",
+		colors: [
+			"#FBFBFA",
+			"#C6D5D8",
+			"#536B6D",
+			"#17232A",
+			"#73121E",
+			"#74957A",
+			"#2E4B39"
+		]
+	),
+	Palette(
+		name: "Spooky",
+		colors: [
+			"#3D0E22",
+			"#862747",
+			"#F2A66C",
+			"#E5732D",
+			"#34234D",
+			"#473074",
+			"#C9B9E0"
+		]
+	),
+	
+	Palette(
+		name: "Ocean",
+		colors: [
+			"#023450",
+			"#076194",
+			"#6493AD",
+			"#72ACAE",
+			"#B4DADB",
+			"#F2F1F7",
+			"#B2AFCC"
+		]
+	),
+	Palette(
+		name: "Greens",
+		colors: [
+			"#DFEEC5",
+			"#BAD87C",
+			"#BDFF00",
+			"#73DB21",
+			"#55B808",
+			"#188B46",
+			"#01672A"
+		]
+	),
+	Palette(
+		name: "Skin",
+		colors: [
+			"#F9F4F0",
+			"#FAEBDE",
+			"#F8ECCF",
+			"#E6C6A2",
+			"#795A3D",
+			"#5F3D1F",
+			"#402812"
+		]
+	)
 ]
